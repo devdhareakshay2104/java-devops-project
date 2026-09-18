acfaab42249eee5057ebfc787605421f5c1b17f1
devdhareakshay2104 <devdhareakshay2104@gmail.com>
2026-09-18 08:05:19 +0530

    Initial Java Maven project setup

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class App {
+
+    public static String getMessage() {
+        return "Java DevOps Project";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Java Maven CI/CD Application");
+        System.out.println("=================================");
+        System.out.println("Application is running successfully!");
+        System.out.println(getMessage());
+
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void applicationMessageTest() {
+
+        assertEquals("Java DevOps Project", App.getMessage());
+
+    }
+}
