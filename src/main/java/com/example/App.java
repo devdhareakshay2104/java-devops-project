d3b2e21afdccc1bf76953b8a14ad8a4b91746cb3
devdhareakshay2104 <devdhareakshay2104@gmail.com>
2026-09-18 08:11:04 +0530

    Improve Java application information

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,13 +6,18 @@ public class App {
         return "Java DevOps Project";
     }
 
+    public static String getApplicationName() {
+        return "Java Maven CI/CD Application";
+    }
+
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println(" Java Maven CI/CD Application");
+        System.out.println(" " + getApplicationName());
         System.out.println("=================================");
         System.out.println("Application is running successfully!");
         System.out.println(getMessage());
+        System.out.println("Version: 1.0");
 
     }
 }
